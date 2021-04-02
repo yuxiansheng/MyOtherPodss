8ba56b24a2a0c3aa6bbdfa5fe70a61d0c8ef2cf4
TengSir <zhangteng@allinmd.cn>
2021-04-02 17:49:24 +0800

    add file

---
+++ Example/Pods/Target Support Files/MyOtherPodss/MyOtherPodss-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyOtherPodss : NSObject
+@end
+@implementation PodsDummy_MyOtherPodss
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyOtherPodss_Example/Pods-MyOtherPodss_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyOtherPodss_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyOtherPodss_Example
+@end
